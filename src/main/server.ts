1c63be357d33310350f66e47b97cae58dd8d0fbd
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-22 15:23:47 -0300

    feat: init server file with example

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,17 @@
-console.log('server');
+import express from 'express';
+
+const PORT = 3333;
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  return response.json({ message: 'Hello World - NLW04' });
+});
+
+app.post('/', (request, response) => {
+  return response.json({ message: 'Os dados foram salvos com sucesso!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`[*] Server running at ${PORT}`);
+});
