c28f8114c6c03a90d8e7ec0861a1bdde53517147
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-24 14:16:49 -0300

    fix: remove script posttest

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 
-import '../database';
+import createConnection from '../database';
 
 import { userRoutes } from '../routes/user';
 
+createConnection();
 const app = express();
 
 app.use(express.json());
