c60dd468fc4f408d20dee42671bee6ceacbeb949
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-25 11:37:00 -0300

    feat: add ManyToOne

--- src/models/survey-user.ts
+++ src/models/survey-user.ts
@@ -2,11 +2,16 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
 import { v4 as uuid } from 'uuid';
+import { Survey } from './survey';
+
+import { User } from './user';
 
 @Entity('SURVEYS_USERS')
 export class SurveyUser {
@@ -16,8 +21,16 @@ export class SurveyUser {
   @Column()
   user_id: string;
 
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
   @Column()
   survey_id: string;
+
+  @ManyToOne(() => Survey)
+  @JoinColumn({ name: 'survey_id' })
+  survey: Survey;
 
   @Column()
   value: number;
