89de3b9374dcde93d6b4190c6add29d9ffd75a00
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-25 09:47:55 -0300

    feat: create SurveyUser model

---
+++ src/models/survey-user.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuid } from 'uuid';
+
+@Entity('SURVEYS_USERS')
+export class SurveyUser {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  survey_id: string;
+
+  @Column()
+  value: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
