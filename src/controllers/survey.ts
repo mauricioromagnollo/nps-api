38e174ddefd2b3471a19355dbd2fa40cca179345
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-26 18:21:25 -0300

    fix: add validation for title and description

--- src/controllers/survey.ts
+++ src/controllers/survey.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 
 import { SurveyRepository } from '@/repositories/survey';
+import { AppError } from '@/errors/app-error';
 
 export class SurveyController {
   async create(request: Request, response: Response): Promise<Response> {
     const { title, description } = request.body;
+
+    const schema = yup.object().shape({
+      title: yup.string().required(),
+      description: yup.string().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
+    }
 
     const surveyRepository = getCustomRepository(SurveyRepository);
 
