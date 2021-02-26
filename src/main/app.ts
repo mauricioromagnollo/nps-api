02a223acba10eb31c96ccebd16b730d61f8b8877
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-26 11:02:36 -0300

    feat: add middleware to catch errors

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,16 +1,33 @@
 import 'reflect-metadata';
 import 'module-alias/register';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import 'dotenv/config';
 
 import createConnection from '@/database';
 
 import { routes } from '@/routes';
+import { AppError } from '@/errors/app-error';
 
 createConnection();
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: `Internal server error ${err.message}`,
+    });
+  },
+);
 
 export { app };
