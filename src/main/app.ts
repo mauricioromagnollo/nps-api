d198910d44b9f9fcde675270184f1fa275d0e306
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-26 11:06:38 -0300

    fix: add _ to request parameter

--- src/main/app.ts
+++ src/main/app.ts
@@ -15,19 +15,17 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: 'Error',
-      message: `Internal server error ${err.message}`,
+app.use((err: Error, _: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
-  },
-);
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error ${err.message}`,
+  });
+});
 
 export { app };
