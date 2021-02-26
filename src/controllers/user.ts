0a10ff41fffd27669753251d18fac1ffa99312b6
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-26 10:39:20 -0300

    feat: add validation with yup

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
 
 import { UserRepository } from '@/repositories/user';
 
 export class UserController {
   async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
 
     const usersRepository = getCustomRepository(UserRepository);
 
