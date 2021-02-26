f5e87f009eacf7343f59037e7782bc38630d167b
x0n4d0 <romagnolimauricio@gmail.com>
2021-02-26 10:19:23 -0300

    feat: add nps route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { UserController } from '@/controllers/user';
 import { SurveyController } from '@/controllers/survey';
 import { SendMailController } from '@/controllers/send-mail';
 import { AnswerController } from '@/controllers/answer';
+import { NpsController } from '@/controllers/nps';
 
 const routes = Router();
 
@@ -11,6 +12,7 @@ const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
 const answerController = new AnswerController();
+const npsController = new NpsController();
 
 routes.post('/users', userController.create);
 
@@ -20,5 +22,7 @@ routes.get('/surveys', surveyController.show);
 routes.post('/sendmail', sendMailController.execute);
 
 routes.get('/answer/:value', answerController.execute);
+
+routes.get('/nps/:survey_id', npsController.execute);
 
 export { routes };
